cb143d448e52982acb38050d36626bf9464cf477
daibang2014 <nguyenquochoang57@gmail.com>
2024-07-19 01:29:30 +0700

    add test admin

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,29 @@
+// HomePage.java
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HomePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(id = "welcome-message")
+    private WebElement welcomeMessage;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getWelcomeMessage() {
+        return wait.until(ExpectedConditions.visibilityOf(welcomeMessage)).getText();
+    }
+}
